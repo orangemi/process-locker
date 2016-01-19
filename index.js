a0a642432d1d8d967e015ceb40d45969a54a9d4e
Orange Mi <orangemiwj@gmail.com>
2016-01-19 13:10:45 +0800

    no yield

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 'use strict'
 
 const fs = require('fs')
-const thunk = require('thunks')()
 const debug = require('debug')('process-locker')
 const thunkRedis = require('thunk-redis')
 const luaAddSubScript = fs.readFileSync(__dirname + '/lua/add_sub.lua', {encoding: 'utf8'})
@@ -18,10 +17,10 @@ function noop () {}
 function Locker (options) {
   options = options || {}
 
-  let list = Object.create(null)
-  let logger, redis, redisSub, redisPrefix, channel, resultTimeout, lockTimeout, processTimeout
+  var list = Object.create(null)
+  var logger, redis, redisSub, redisPrefix, channel, resultTimeout, lockTimeout, processTimeout
 
-  let redisConfig = options.redisConfig || ['localhost:6379']
+  var redisConfig = options.redisConfig || ['localhost:6379']
   redisPrefix = options.redisPrefix || 'locker'
   channel = redisPrefix + ':' + (options.channel || 'channel')
   resultTimeout = options.resultTimeout || 30 * 60 * 1000
@@ -34,7 +33,7 @@ function Locker (options) {
 
   initListener(redis)
   initListener(redisSub)
-  let locker = {}
+  var locker = {}
   locker.redis = redis
 
   function initListener (client) {
@@ -54,7 +53,7 @@ function Locker (options) {
 
   redisSub.on('message', function (chl, json) {
     if (chl !== channel) return
-    let key, value
+    var key, value
 
     try {
       json = JSON.parse(json)
@@ -67,77 +66,77 @@ function Locker (options) {
     }
 
     debug('process key : %s', key)
+    list[key] = list[key] || []
 
-    let callbackList = list[key] || []
-    let callback
-    let count = 0
-
-    while ((callback = callbackList.shift())) {
-      debug('process callback %s %d', key, count++)
-      callback(null, {status: Locker.DONE, result: value})
-    }
+    list[key].map(function (callback) {
+      debug('process callback %s', key)
+      process.nextTick(function () {
+        callback(null, {status: Locker.DONE, result: value})
+      })
+    })
     delete list[key]
   }).subscribe(channel)()
 
   locker.request = function (key, _options) {
     _options = _options || {}
     return function (callback) {
-      thunk(function *() {
-        let callbackList = list[key] = list[key] || []
-        let timer
-        let _callback = callback
-        callback = function () {
-          if (timer) {
-            clearTimeout(timer)
-            timer = null
-          }
-          _callback.apply(null, arguments)
-        }
+      list[key] = list[key] || []
+      var timer
+      var _callback = callback || noop
+      callback = function () {
+        if (timer) clearTimeout(timer)
+        _callback.apply(this, arguments)
+      }
 
-        callbackList.push(callback)
+      list[key].push(callback)
 
-        let result
-        let redisKey = `${redisPrefix}:${key}`
-        if (_options.get !== true) {
-          result = yield redis.evalauto(luaAddSubScript, 1, redisKey, lockTimeout)
-        } else {
-          result = yield redis.get(redisKey)
-        }
+      var nextFn
+      var redisKey = `${redisPrefix}:${key}`
+      if (_options.get !== true) {
+        nextFn = redis.evalauto(luaAddSubScript, 1, redisKey, lockTimeout)
+      } else {
+        nextFn = redis.get(redisKey)
+      }
+      nextFn(function (err, result) {
+        if (err) return callback(err)
         debug('redis %s response %s', redisKey, result)
 
         timer = setTimeout(function () {
-          let index = callbackList.indexOf(callback)
-          if (index === -1) return
-          callbackList.splice(index, 1)
-          if (!callbackList.length) delete list[key]
+          deleteCallback(key, callback)
           callback(new Error('process Timeout'))
         }, processTimeout)
 
-        if (result === Locker.WAIT) {
-          return
-        }
+        if (result === Locker.WAIT) return
 
-        let index = callbackList.indexOf(callback)
-        if (index === -1) return
-        callbackList.splice(index, 1)
-        if (!callbackList.length) delete list[key]
-
+        deleteCallback(key, callback)
         if (result === Locker.LOCKED) {
-          callback(null, {status: Locker.LOCKED})
+          return callback(null, {status: Locker.LOCKED})
         } else {
-          callback(null, {status: Locker.DONE, result: JSON.parse(result)})
+          return callback(null, {status: Locker.DONE, result: JSON.parse(result)})
         }
-      })()
+      })
     }
   }
 
-  locker.publish = function *(key, value, timeout) {
-    timeout = timeout || resultTimeout
-    debug('publish %s', key)
-    let redisKey = `${redisPrefix}:${key}`
-    let result = yield redis.evalauto(luaDelPubScript, 2, redisKey, channel, JSON.stringify(value), timeout)
-    debug('publish result %s', result)
-    return result
+  locker.publish = function (key, value, timeout) {
+    return function (callback) {
+      callback = callback || noop
+      timeout = timeout || resultTimeout
+      debug('publish %s', key)
+      var redisKey = `${redisPrefix}:${key}`
+      redis.evalauto(luaDelPubScript, 2, redisKey, channel, JSON.stringify(value), timeout)(function (err, result) {
+        if (err) return callback(err)
+        debug('publish result %s', result)
+        callback(null, result)
+      })
+    }
+  }
+
+  function deleteCallback (key, callback) {
+    var index = list[key].indexOf(callback)
+    if (index === -1) return
+    list[key].splice(index, 1)
+    if (!list[key].length) delete list[key]
   }
 
   return locker
