4bc7a92feabf1e350bf7874d2154024cdcac215d
Orange Mi <orangemiwj@gmail.com>
2019-01-21 16:56:04 +0800

    issue: use key directly in lua script for ali-redis cluster (#2)

--- lua/add_sub.lua
+++ lua/add_sub.lua
@@ -2,13 +2,13 @@
 -- KEYS[1] target fileKey
 -- ARGV[1] key time out
 
-local fileKey = KEYS[1]
+-- local fileKey = KEYS[1]
 local timeout = ARGV[1]
 
-local result = redis.call('GET', fileKey)
+local result = redis.call('GET', KEYS[1])
 
 if not result then
-  redis.call('PSETEX', fileKey, timeout, 'WAIT')
+  redis.call('PSETEX', KEYS[1], timeout, 'WAIT')
   return 'LOCKED'
 else
   return result
--- lua/del_pub.lua
+++ lua/del_pub.lua
@@ -4,12 +4,12 @@
 -- ARGV[1] convert result
 -- ARGV[2] result time out
 
-local key = KEYS[1]
-local channel = KEYS[2]
+-- local key = KEYS[1]
+-- local channel = KEYS[2]
 local value = ARGV[1]
 local timeout = ARGV[2]
 
-redis.call('PSETEX', key, timeout, value)
-local result = redis.call('PUBLISH', channel, cjson.encode({key=key, value=value}))
+redis.call('PSETEX', KEYS[1], timeout, value)
+local result = redis.call('PUBLISH', KEYS[2], cjson.encode({key=KEYS[1], value=value}))
 
 return result
